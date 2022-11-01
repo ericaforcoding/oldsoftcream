20c6d684785bc1adef08a46bb407e99271852d29
BuildEnough <rhtmaehcl878@naver.com>
2022-11-01 11:18:16 +0900

    base mk1

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, redirect
+from django.urls import path
 from . import views
 
 app_name = 'articles'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('create/', views.create, name='create'),
 ]
