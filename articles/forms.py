e7107ee13241c309780c65f1df4f5f169cbf04d7
dortkthf <dortkthf@gmail.com>
2022-11-03 17:37:49 +0900

    make profile

--- articles/forms.py
+++ articles/forms.py
@@ -2,15 +2,30 @@ from xml.etree.ElementTree import Comment
 from django import forms
 from .models import Articles, Comment
 
+
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Articles
-        fields = ['title', 'content', 'image']
+        fields = ["title", "content", "image"]
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content']
+        fields = ["content"]
+        labels = {
+            "content": " ",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+
+        self.fields["content"].widget.attrs = {
+            "class": "form-control me-3",
+            "placeholder": "댓글을 입력해주세요",
+            "rows": 1,
+        }
+
 
 # class ImageForm(forms.ModelForm):
 #     class Meta:
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Articles
+from .models import Articles, Comment
 from .forms import ArticleForm, CommentForm
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import get_object_or_404
@@ -86,3 +86,31 @@ def like(request, pk):
         article.like_users.add(request.user)
         is_liked = True
     return JsonResponse({"isLiked": is_liked, "likeCount": article.like_users.count()})
+
+
+def comment(request, pk):
+    article = Articles.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.articles = article
+        comment.user = request.user
+        comment.save()
+        context = {
+            "content": comment.content,
+            "userName": comment.user.username,
+            "userImgUrl": comment.user.profile.image.url,
+            "created": comment.create_at,
+        }
+        return JsonResponse(context)
+    return redirect("articles:detail", article.pk)
+
+
+def comment_d(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.delete()
+    return redirect("articles:detail", comment.articles.pk)
+
+
+def append(request):
+    return render(request, "append.html")
